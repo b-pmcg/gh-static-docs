953b7b4d2eceb79f21ccf5fda32a7c0a96dec356
b-pmcg <bain.pmcg@gmail.com>
2020-06-03 16:20:08 +0000

    Deploying to master from  @ 3192f638f1db119a4b71487284360f7c7c592c5d 🚀

--- components/Lead.js
+++ components/Lead.js
@@ -13,7 +13,7 @@ const Lead = () => {
       }}
     >
       <Heading variant="largeHeading" as="h1">
-        IT’S TIME TO BUILD234
+        IT’S TIME TO BUILD567
       </Heading>
       <Text variant="bodyLarge" sx={{ my: 3 }}>
         Every Western institution was unprepared for the coronavirus pandemic,
