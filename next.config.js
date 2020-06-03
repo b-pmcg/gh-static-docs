3dfb5f6a2e9a87fd6c19f0e65bce9c2d07e8e770
Phil Bain <bain.pmcg@gmail.com>
2020-06-03 12:57:40 +0200

    fix path

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ const slug = require('remark-slug');
 const mdxTableOfContents = require('./tocModule');
 
 const isProd = (process.env.NODE_ENV || 'production') === 'production';
-const repoName = 'gh-static-docs';
+const repoName = '/gh-static-docs';
+console.log('ENV:', process.env.NODE_ENV);
 
 const withMDX = require('@next/mdx')({
   options: {
