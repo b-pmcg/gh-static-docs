7ada83f90ee21d3d1537c8d822309f7bdb8465ea
Phil Bain <bain.pmcg@gmail.com>
2020-06-03 12:44:52 +0200

    add exportPathMap to next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const slug = require('remark-slug');
 const mdxTableOfContents = require('./tocModule');
 
+const isProd = (process.env.NODE_ENV || 'production') === 'production';
+const repoName = 'gh-static-docs';
+
 const withMDX = require('@next/mdx')({
   options: {
     remarkPlugins: [slug],
@@ -9,4 +12,8 @@ const withMDX = require('@next/mdx')({
 });
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+  exportPathMap: () => ({
+    '/': { page: '/' },
+  }),
+  assetPrefix: isProd ? repoName : '',
 });
