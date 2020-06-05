70559fabdc3f28a66ca263359195b4af1ad23482
Phil Bain <bain.pmcg@gmail.com>
2020-06-05 17:00:01 +0200

    update sanity check

--- components/Lead.js
+++ components/Lead.js
@@ -13,7 +13,7 @@ const Lead = () => {
       }}
     >
       <Heading variant="largeHeading" as="h1">
-        IT’S TIME TO BUILD567
+        IT’S TIME TO BUILD8910
       </Heading>
       <Text variant="bodyLarge" sx={{ my: 3 }}>
         Every Western institution was unprepared for the coronavirus pandemic,
